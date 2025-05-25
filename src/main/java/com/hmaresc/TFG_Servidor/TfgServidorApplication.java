a2266f13784eccdb542d64eb9c86276d8511c96b
Rekken4848 <hugomarescrihuela@gmail.com>
2025-05-25 02:34:18 +0200

    Spring Boot Base

---
+++ src/main/java/com/hmaresc/TFG_Servidor/TfgServidorApplication.java
@@ -0,0 +1,13 @@
+package com.hmaresc.TFG_Servidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgServidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfgServidorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hmaresc/TFG_Servidor/TfgServidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmaresc.TFG_Servidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgServidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
